1cd0352041b102d997f87c3d3066d817142a2436
Yaroslav Moroz <yaroslav.moroz91@outlook.com>
2018-07-18 22:56:35 +0300

    clear

--- md5scannerzgo.go
+++ md5scannerzgo.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"crypto/md5"
+	"encoding/csv"
 	"encoding/hex"
 	"fmt"
 	"io"
@@ -11,20 +12,21 @@ import (
 	"os/exec"
 	"regexp"
 	"strings"
-	"encoding/csv"
 	"time"
 )
+
 var gmylog string
 var mybuffer = "C:\\Windows\\Temp\\md5utils"
 var gto string
 var result [][]string
-func checkError(message string, err error){
+
+func checkError(message string, err error) {
 	if err != nil {
-		log.Fatal(message,err)
+		log.Fatal(message, err)
 	}
 }
-func strs(s ...string)string{
-	return strings.Join(s," ")
+func strs(s ...string) string {
+	return strings.Join(s, " ")
 }
 func inBuffer(s string) bool {
 	return strings.Contains(s, mybuffer)
@@ -38,31 +40,31 @@ func myexe(s ...string) {
 	app := s[0]
 	args := s[1:len(s)]
 	out, err := exec.Command(app, args...).Output()
-	checkError("Error: myexe cannot run "+strs(s...),err)
+	checkError("Error: myexe cannot run "+strs(s...), err)
 	fmt.Printf("%s", out)
 }
 func myrmtree(imypath string) {
 	os.RemoveAll(imypath)
 }
 func p(s ...string) string {
-	s1:= strs(s...)
+	s1 := strs(s...)
 	fmt.Println(s1)
-	gmylog=gmylog+s1+"\r\n"
+	gmylog = gmylog + s1 + "\r\n"
 	return s1
 }
 func mymd5(xfile string) string {
 	f, err := os.Open(xfile)
-	checkError("Error: mymd5 cannot open file "+xfile,err)
+	checkError("Error: mymd5 cannot open file "+xfile, err)
 	defer f.Close()
 	h := md5.New()
 	_, err2 := io.Copy(h, f)
-	checkError("Error: mymd5 cannot calculate md5 for file "+xfile,err2)
+	checkError("Error: mymd5 cannot calculate md5 for file "+xfile, err2)
 	return strings.ToUpper(hex.EncodeToString(h.Sum(nil)))
 }
 func mytype(ipath string) string {
 	var ytype string
 	fi, err := os.Lstat(ipath)
-	checkError("Error: mytype cannot os.Lstat("+ipath+")",err)
+	checkError("Error: mytype cannot os.Lstat("+ipath+")", err)
 	switch mode := fi.Mode(); {
 	case mode.IsRegular():
 		ytype = "afile"
@@ -73,7 +75,7 @@ func mytype(ipath string) string {
 	}
 	if ytype == "afile" {
 		matched, err2 := regexp.MatchString(".*\\.(7z|zip|rar)$", ipath)
-		checkError("Error: mytype cannot regexp.MatchString(regex, "+ipath+")",err2)
+		checkError("Error: mytype cannot regexp.MatchString(regex, "+ipath+")", err2)
 		if matched {
 			ytype = "archive"
 		} else {
@@ -85,7 +87,7 @@ func mytype(ipath string) string {
 func myfiles(ipath string) []string {
 	var result0 []string
 	files, err := ioutil.ReadDir(ipath)
-	checkError("Error: myfiles cannot ioutil.ReadDir("+ipath+")",err)
+	checkError("Error: myfiles cannot ioutil.ReadDir("+ipath+")", err)
 	for _, f := range files {
 		result0 = append(result0, f.Name())
 	}
@@ -146,23 +148,21 @@ func main() {
 		myfrom := os.Args[2]
 		result = append(result, []string{"path", "name", "md5"})
 		readz(myfrom)
-		fmt.Println(result)
-		fmt.Println(len(result))
-		amyfrom:=strings.Split(myfrom,"\\")
-		mycsv:=gto+"\\"+amyfrom[len(amyfrom)-1]+" ("+time.Now().Format("02.01.2006")+").csv"
-		file,err:=os.Create(mycsv)
-		checkError("Error: main cannot os.Create("+mycsv+")",err)
+		amyfrom := strings.Split(myfrom, "\\")
+		mycsv := gto + "\\" + amyfrom[len(amyfrom)-1] + " (" + time.Now().Format("02.01.2006") + ").csv"
+		file, err := os.Create(mycsv)
+		checkError("Error: main cannot os.Create("+mycsv+")", err)
 		defer file.Close()
-		writer:=csv.NewWriter(file)
+		writer := csv.NewWriter(file)
 		defer writer.Flush()
-		for _,value:=range result {
-			err:=writer.Write(value)
-			checkError("Error: main cannot writer.Write("+strs(value...)+")",err)
+		for _, value := range result {
+			err := writer.Write(value)
+			checkError("Error: main cannot writer.Write("+strs(value...)+")", err)
 		}
-		mylog:=gto+"\\mylog.txt"
-		d:=[]byte(gmylog)
-		fmt.Println(gmylog)
-		ioutil.WriteFile(mylog,d,0777)
+		mylog := gto + "\\mylog.txt"
+		p("\nfinish")
+		d := []byte(gmylog)
+		ioutil.WriteFile(mylog, d, 0777)
 	case "isEmpty":
 		if isEmpty(os.Args[2]) {
 			p("true")
